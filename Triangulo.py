b8d09b3c4a70f436a8b304f7bde489e85480d496
luckas <luckas.strnadalvarez@gmail.com>
2021-11-11 21:24:19 -0300

    ajustes leves al triangulo para que sea mas rapido y amplio su movimiento

--- Triangulo.py
+++ Triangulo.py
@@ -14,8 +14,8 @@ class Triangulo(pygame.sprite.Sprite):
         self.rect.y = pos[1]
 
     def dibujar(self, pantalla, ahora):
-        pantalla.blit(self.image, [self.rect.x - 5, self.rect.y + self.cont - 35])
-        if ahora - self.tiempo > 50:
+        pantalla.blit(self.image, [self.rect.x, self.rect.y + self.cont - 35])
+        if ahora - self.tiempo > 20:
             # cambia imagenes
             self.image = self.image = pygame.image.load(datos.abrir(
                 datos.carpeta_tanke, "turno.png"))
@@ -26,9 +26,9 @@ class Triangulo(pygame.sprite.Sprite):
             else:
                 self.cont += 1
 
-            if self.cont <= -4:
+            if self.cont <= -5:
                 self.sube = False
-            if self.cont >= 4:
+            if self.cont >= 5:
                 self.sube = True
 
     def mover(self, pos):
